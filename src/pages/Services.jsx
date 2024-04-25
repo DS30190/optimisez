84a0d23b1d570bf48513a5dbd1bbf641109c0985
David <saumet.david@orange.fr>
2024-04-25 19:22:09 +0200

    add serv1

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -2,7 +2,21 @@
 const Services = () => {
     return (
         <div className="total">
-            <h1>Services</h1>
+            <h1>MON OFFRE DE SERVICES</h1>
+            <p>Voici les prestations sur lesquelles je peux intervenir.</p>
+            <p className="line"></p>
+            <div class="row">
+            <div class="col1 col-md-3 mb-3">
+        Contenu de la colonne 1
+    </div>
+    <div class="col2 col-md-3 mb-3">
+        Contenu de la colonne 2
+    </div>
+    <div class="col3 col-md-3 mb-3">
+        Contenu de la colonne 3
+    </div>
+</div>
+
         </div>
     )
 }
