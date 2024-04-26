59b36c22fc2e4e47066ce639fba29aeb47b1e379
David <saumet.david@orange.fr>
2024-04-26 17:00:06 +0200

    add blog

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -2,8 +2,101 @@
 const Blog = () => {
     return (
         <div>
-            <h1>Blog</h1>
+            
+            <img class="banner" src="/images/banner.jpg" alt="bannière"></img>
+            <h1>BLOG</h1>
+            <p>Retrouvez ici quelques articles sur le développement web.</p>
+            <p className="line"></p>
+    <div class="row mx-auto">
+        
+        <div class="col-lg-4 col-md-6 col-sm-12 mb-4">
+            <div class="card">
+                <img src="/images/blog/coder.jpg" class="card-img-top" alt="Article"></img>
+                <div class="card-body">
+                    <h2 class="card-title">Coder son site en HTML/CSS</h2>
+                    <p class="card-text">Some quick example text to build on the card title 
+                    and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-blog">Lire la suite</a>
+                    <div class="bg-gray">
+                <p class="card-text">Publié le 22 août 2022</p> 
+            </div>
+                </div>
+            </div>
         </div>
+        <div class="col-lg-4 col-md-6 col-sm-12 mb-4">
+            <div class="card">
+            <img src="/images/blog/croissance.jpg" class="card-img-top" alt="Article"></img>
+                <div class="card-body">
+                    <h2 class="card-title">Vendre ses produits sur le web</h2>
+                    <p class="card-text">Some quick example text to build on the card title 
+                    and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-blog">Lire la suite</a>
+                    <div class="bg-gray">
+                <p class="card-text">Publié le 20 août 2022</p> 
+            </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-4 col-md-6 col-sm-12 mb-4">
+            <div class="card">
+            <img src="/images/blog/google.jpg" class="card-img-top" alt="Article"></img>
+                <div class="card-body">
+                    <h2 class="card-title">Se positionner sur Google</h2>
+                    <p class="card-text">Some quick example text to build on the card title 
+                    and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-blog">Lire la suite</a>
+                    <div class="bg-gray">
+                <p class="card-text">Publié le 1 août 2022</p> 
+            </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-4 col-md-6 col-sm-12 mb-4">
+            <div class="card">
+            <img src="/images/blog/screens.jpg" class="card-img-top" alt="Article"></img>
+                <div class="card-body">
+                    <h2 class="card-title">Coder en responsive design</h2>
+                    <p class="card-text">Some quick example text to build on the card title 
+                    and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-blog">Lire la suite</a>
+                    <div class="bg-gray">
+                <p class="card-text">Publié le 31 juillet 2022</p> 
+            </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-4 col-md-6 col-sm-12 mb-4">
+            <div class="card">
+            <img src="/images/blog/seo.jpg" class="card-img-top" alt="Article"></img>
+                <div class="card-body">
+                    <h2 class="card-title">Techniques de référencement</h2>
+                    <p class="card-text">Some quick example text to build on the card title 
+                    and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-blog">Lire la suite</a>
+                    <div class="bg-gray">
+                <p class="card-text">Publié le 30 juillet 2022</p> 
+            </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-4 col-md-6 col-sm-12 mb-4">
+            <div class="card">
+            <img src="/images/blog/technos.png" class="card-img-top" alt="Article"></img>
+                <div class="card-body">
+                    <h2 class="card-title">Apprendre à coder</h2>
+                    <p class="card-text">Some quick example text to build on the card title 
+                    and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-blog">Lire la suite</a>
+                    <div class="bg-gray">
+                <p class="card-text">Publié le 12 juillet 2022</p> 
+            </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+      
     )
 }
 export default Blog;
